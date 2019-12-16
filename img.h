4d7f7f518ffd7b9618f4d65943350f66de010fa6
Alexander Renz <alex@imkerle.de>
2019-12-16 07:29:48 +0100

    Add example image

---
+++ img.h
@@ -0,0 +1,22 @@
+/*
+ * auto-generated file by jpeg-to-rgb565 sv0b
+ * for more information check:
+ * https://github.com/gimbas/jpeg-to-rgb565
+ *
+ * format:
+ * usImg[0] = image width
+ * usImg[1] = image heigth
+ * usImg[2:last] = 16bit rgb565 pixel color from top left to bottom right
+ *
+ * resolution: 45w x 45h
+ * n symbols(16bit): 2025
+ * size(bits): 32Kbit
+ * size(bytes): 3KiB
+ */
+
+#ifndef	__IMG_H_
+#define	__IMG_H_
+
+extern const uint16_t usImg[];
+
+#endif	// __IMG_H_
